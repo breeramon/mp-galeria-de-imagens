f7812032ab8064623ac636d70a3f76f68bacdad7
Breno Santana <breeramon@gmail.com>
2025-04-15 18:08:59 -0300

    quase pronto, só falta organizar o html e o css das fotos

---
+++ script.js
@@ -0,0 +1,45 @@
+import { images } from "./dados.js";
+const galleryContainer = document.querySelector('#gallery-container');
+const loadMoreButton = document.querySelector('#load-more');
+console.log(images);
+const imagesPerPage = 4;
+let currentImageIndex = 0;
+
+function loadImages() {
+    const pageImages = images.slice(currentImageIndex, currentImageIndex + imagesPerPage);
+    console.log(pageImages);
+
+    pageImages.forEach(images => {
+        const galleryItem = document.createElement('div');
+        galleryItem.className = `gallery-item ${images.wide ? 'wide' : ''}`;
+
+        const imgItem = document.createElement('img');
+        imgItem.src = images.src;
+        imgItem.alt = images.alt;
+
+        galleryItem.appendChild(imgItem);
+        galleryContainer.appendChild(galleryItem);
+    });
+
+    currentImageIndex += imagesPerPage;
+    if(currentImageIndex >= images.length) {
+        loadMoreButton.style.display = 'none';
+    }
+}
+
+loadImages();
+loadMoreButton.addEventListener('click', loadImages);
+
+window.addEventListener('scroll', () => {
+    const viewportHeight = window.innerHeight;
+
+    const scrollPosition = window.scrollY;
+
+    const pageHeight = document.body.offsetHeight;
+
+    const offset = pageHeight - 100;
+
+    if(viewportHeight + scrollPosition >= offset) {
+        loadImages();
+    }
+});
